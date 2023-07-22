ceca622a31a96d2d82f8fd06ef65744f1f859f79
Alaa Ebrahim <alaaebrahim387@gmail.com>
2023-07-22 17:33:31 +0300

    Added PayPayl Payment Gateway (not completed yet)

---
+++ ShopOnline.Web/Pages/CheckoutBase.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using ShopOnline.Models.Dtos;
+using ShopOnline.Web.Services.Contracts;
+
+namespace ShopOnline.Web.Pages
+{
+	public class CheckoutBase: ComponentBase
+	{
+        [Inject]
+        public IJSRuntime JSRuntime{ get; set; }
+
+        [Inject]
+        public IShoppingCartService ShoppingCartService { get; set; }
+
+        public IEnumerable<CartItemDto> CartItems{ get; set; }
+
+        public decimal PaymentAmount { get; set; }
+        public string PaymentDescription { get; set; }
+        public int TotalQty { get; set; }
+
+        protected override async Task OnInitializedAsync()
+		{
+			try
+			{
+				CartItems = await ShoppingCartService.GetAll(1);
+
+				if (CartItems != null)
+				{
+					Guid orderId = Guid.NewGuid();
+					PaymentAmount = CartItems.Sum(x => x.TotalPrice);
+					TotalQty = CartItems.Sum(x => x.Qty);
+					PaymentDescription = $"O_{orderId}_{1}";
+				}
+			}
+			catch (Exception)
+			{
+
+				throw;
+			}
+		}
+
+		protected override async Task OnAfterRenderAsync(bool firstRender)
+		{
+			if (firstRender)
+			{
+				await JSRuntime.InvokeVoidAsync("initPayPalButton");
+			}
+		}
+	}
+}
